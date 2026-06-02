3667d1775b3cfc07314709b9f0ebf577a75550bd
Viviane Aguiar <vivianeaguiarc@outlook.com>
2026-06-02 14:46:08 -0300

    feat: add express setup with hello world route

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const name = 'John Doe'
-console.log(name)
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!')
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
